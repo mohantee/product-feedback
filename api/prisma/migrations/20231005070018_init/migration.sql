26df0c89aa757df848e3f0080a0594e93e818ec0
Saurav Mohanty <sauravmohantee@gmail.com>
2023-10-05 12:32:29 +0530

    feat: add upvotes model and change prisma schema

--- api/prisma/migrations/20231005070018_init/migration.sql
+++ api/prisma/migrations/20231005070018_init/migration.sql
@@ -1,7 +1,6 @@
 -- CreateTable
 CREATE TABLE "Feedback" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "upvotes" INTEGER DEFAULT 0,
     "title" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
@@ -14,11 +13,19 @@ CREATE TABLE "Feedback" (
 CREATE TABLE "Comment" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "content" TEXT NOT NULL,
-    "feedbackId" INTEGER NOT NULL,
+    "feedbackId" INTEGER,
     "userId" TEXT NOT NULL,
     "predecessorId" INTEGER,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
-    CONSTRAINT "Comment_feedbackId_fkey" FOREIGN KEY ("feedbackId") REFERENCES "Feedback" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Comment_feedbackId_fkey" FOREIGN KEY ("feedbackId") REFERENCES "Feedback" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
     CONSTRAINT "Comment_predecessorId_fkey" FOREIGN KEY ("predecessorId") REFERENCES "Comment" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Upvote" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" TEXT NOT NULL,
+    "feedbackId" INTEGER NOT NULL,
+    CONSTRAINT "Upvote_feedbackId_fkey" FOREIGN KEY ("feedbackId") REFERENCES "Feedback" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
