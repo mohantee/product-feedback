2c10770768304ffc4d53204bc639de098196d0c6
Saurav Mohanty <sauravmohantee@gmail.com>
2023-10-11 23:34:59 +0530

    chore: download assets

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import postcssNesting from 'postcss-nesting'
+import postcssNesting from "postcss-nesting";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  //@ts-ignore
+  plugins: [react(), svgr({ exportAsDefault: true })],
   css: {
     postcss: {
-      plugins: [postcssNesting]
-    }
-  }
+      plugins: [postcssNesting],
+    },
+  },
 });
